c4e893059bac2f8359135cb8a3a8b5645ba184f5
Thanakrit Ritdej <72487386+sbknine@users.noreply.github.com>
2020-11-23 14:32:36 +0700

    API

--- server.js
+++ server.js
@@ -3,23 +3,21 @@ var express = require('express'); //standard of node.js
 var path = require('path'); //standard of node.js
 var https = require('https'); //standard of node.js
 var http = require('http'); //standard of node.js
-
 var PORT  = process.env.PORT || 5000; //standard of node.js
-
 var app = express(); //standard of node.js
-
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views')); //standard of node.js
 app.set('view engine', 'ejs'); //standard of node.js
-
 app.get('/', function (req, res) {
-    res.render('index');
+    res.render('index', {fname: 'Songsakdi', lName: 'Rongviri'});
 });
-
 app.listen(PORT, function () {
     console.log(`Listening on ${PORT}`)
 });
-
-app.get("/menu/:id", async function (req, res) {
+app.get("/info/:id", async function (req, res) {
     var nameid = req.params.id;
     console.log(nameid);
     const data = await getStudentInfo(nameid);
@@ -36,11 +34,7 @@ app.get("/menu/:id", async function (req, res) {
                 department: j.data.department
             });
     }
-
-
-
 });
-
 app.post("/api", async (req, res) => {
     //const queryParams = req.query;
     //let result = "";
@@ -52,16 +46,15 @@ app.post("/api", async (req, res) => {
         let j = JSON.parse(temp);
         console.log(j);
         if (j.status == true) {
-            res.send(temp);
+            res.render("menu");
             //res.send("Name th: " + j.displayname_th);
         } else {
             res.send('{"status":false}');
         }
-
         // if (j.data.status == true) {
-        //   res.send("สถานะ:  นักศึกษา")
+        //   res.send("ʶҹ�:  �ѡ�֡��")
         // } else (j.data.status)
-        // req.send("สถานะ: พ้นสถาพ")
+        // req.send("ʶҹ�: ��ʶҾ")
     } else {
         res.send('{"status":false}');
     }
@@ -71,7 +64,6 @@ app.post("/api", async (req, res) => {
     // res.send("login fail");
     // }
 });
-
 const getlogin = (userName, password) => {
     return new Promise((resolve, reject) => {
         var options = {
@@ -80,24 +72,20 @@ const getlogin = (userName, password) => {
             'path': '/api/v1/auth/Ad/verify',
             'headers': {
                 'Content-Type': 'application/json',
-                'Application-Key': 'TU649840bd46e34cb0079602276dca0d4276bea27f15635213466f38f4ecffe48600e1c2b47e9308d5dd9a888567de6c00'
+                'Application-Key': 'TUa2e215505b9e2e75472dfe964b5bf566f650b84b1892206d9c26c0669874d7cc00bdf6a7fa9e90853552b48c1cbc6a2d'
             }
         };
-
         var req = https.request(options, (res) => {
             var chunks = [];
-
             res.on("data", function (chunk) {
                 chunks.push(chunk);
             });
-
             res.on("end", function (chunk) {
                 var body = Buffer.concat(chunks);
                 //result = body;
                 resolve(body.toString());
                 //result = chunks;
             });
-
             res.on("error", function (error) {
                 console.error(error);
                 reject(error);
@@ -108,7 +96,6 @@ const getlogin = (userName, password) => {
         req.end();
     });
 };
-
 const getStudentInfo = (username) => {
     return new Promise((resolve, reject) => {
         var options = {
@@ -118,30 +105,26 @@ const getStudentInfo = (username) => {
             headers: {
                 "Content-Type": "application/json",
                 "Application-Key":
-                    "TU649840bd46e34cb0079602276dca0d4276bea27f15635213466f38f4ecffe48600e1c2b47e9308d5dd9a888567de6c00",
+                    "TUa2e215505b9e2e75472dfe964b5bf566f650b84b1892206d9c26c0669874d7cc00bdf6a7fa9e90853552b48c1cbc6a2d",
             },
         };
-
         var req = https.request(options, (res) => {
             var chunks = [];
 
             res.on("data", function (chunk) {
                 chunks.push(chunk);
             });
-
             res.on("end", function (chunk) {
                 var body = Buffer.concat(chunks);
                 //result = body;
                 resolve(body.toString());
                 //result = chunks;
             });
-
             res.on("error", function (error) {
                 console.error(error);
                 reject(error);
             });
         });
-
         req.end();
     });
 };
@@ -154,112 +137,46 @@ app.get('/doc', function (req, res) {
 app.get('/next', function (req, res) {
     res.render('next')
 });
-
-
-
-
-
-
-//var options = {
-//    'method': 'POST',
-//    'hostname': 'restapi.tu.ac.th',
-//    'path': '/api/v1/auth/Ad/verify',
-//    'headers': {
-//        'Content-Type': 'application/json',
-//        'Application-Key': 'TUa4e553b83aa271d3411a4ad88395265801fcfb074110e8b0e03962c01f2aed6ab1662db3a0e1451df7835880c6828fcf'
-//    }
-//};
-
-//var req = https.request(options, function (res) {
-//    var chunks = [];
-
-//    res.on("data", function (chunk) {
-//        chunks.push(chunk);
-//    });
-
-//    res.on("end", function (chunk) {
-//        var body = Buffer.concat(chunks);
-//        console.log(body.toString());
-//    });
-
-//    res.on("error", function (error) {
-//        console.error(error);
-//    });
-//});
-
-
-//var options = {
-//    'method': 'GET',
-//    'hostname': 'restapi.tu.ac.th',
-//    'path': '/api/v2/std/fac/all',
-//    'headers': {
-//        'Content-Type': 'application/json',
-//        'Application-Key': 'TUa4e553b83aa271d3411a4ad88395265801fcfb074110e8b0e03962c01f2aed6ab1662db3a0e1451df7835880c6828fcf'
-//    }
-//};
-
-//var req = https.request(options, function (res) {
-//    var chunks = [];
-
-//    res.on("data", function (chunk) {
-//        chunks.push(chunk);
-//    });
-
-//    res.on("end", function (chunk) {
-//        var body = Buffer.concat(chunks);
-//        console.log(body.toString());
-//    });
-
-//    res.on("error", function (error) {
-//        console.error(error);
-//    });
-//});
-
-//req.end();
-
-
-const options = {
-    hostname: 'jsonplaceholder.typicode.com',
-    path: '/posts/1/comments',
-    method: 'GET',
-    'headers': {
-        'Content-Type': 'application/json',
-    }
-};
-
-function dataCounter(inputs) {
-    let counter = 0;
-    for (const input of inputs) {
-        if (input.postId === 1) {
-            counter += 1;
-            console.log('input.postId:' + input.postId);
-            console.log('input.email:' + input.email);
-        }
-    }
-    return counter;
-};
-
-const req = http.request(options, function(response) {
-    response.setEncoding('utf8');
-    var body = '';
-    response.on('data', chunk => {
-        body += chunk;
-    });
-
-    response.on('end', () => {
-        console.log('body:' + body);
-        var data = JSON.parse(body);
-        console.log('number of posts:' + dataCounter(data));
-        console.log('data:' + data);
-        console.log('data[0]:' + data[0]);
-        console.log('data[0].id:' + data[0].id);
-        console.log('data[0].email:' + data[0].email);
-        console.log('end of GET request');
-    });
+app.get('/condition', function (req, res) {
+    res.render('condition')
 });
-
-
-req.on('error', e => {
-    console.log('Problem with request:', e.message);
-});
-req.end();
+// const options = {
+//     hostname: 'jsonplaceholder.typicode.com',
+//     path: '/posts/1/comments',
+//     method: 'GET',
+//     'headers': {
+//         'Content-Type': 'application/json',
+//     }
+// };
+// function dataCounter(inputs) {
+//     let counter = 0;
+//     for (const input of inputs) {
+//         if (input.postId === 1) {
+//             counter += 1;
+//             console.log('input.postId:' + input.postId);
+//             console.log('input.email:' + input.email);
+//         }
+//     }
+//     return counter;
+// };
+// const req = http.request(options, function(response) {
+//     response.setEncoding('utf8');
+//     var body = '';
+//     response.on('data', chunk => {
+//         body += chunk;
+//     });
+//     response.on('end', () => {
+//         console.log('body:' + body);
+//         var data = JSON.parse(body);
+//         console.log('number of posts:' + dataCounter(data));
+//         console.log('data:' + data);
+//         console.log('data[0]:' + data[0]);
+//         console.log('data[0].id:' + data[0].id);
+//         console.log('data[0].email:' + data[0].email);
+//         console.log('end of GET request');
+//     });
+// });
+// req.on('error', e => {
+//     console.log('Problem with request:', e.message);
+// });
+// req.end();
